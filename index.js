5a512960fb7e2c136dbe987d6523d79d4a7bb46c
Zach Lefler <leaving.valhalla@gmail.com>
2022-02-25 16:41:56 -0800

    initial commit

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 function addingEventListener() {
-  const input = document.getElementById("input");
-  input.addEventListener("click", function () {
-    alert("I've been clicked!");
-  });
+  const input = document.getElementById('input');
+  input.addEventListener('click', () => alert("I've been clicked!"));
 }
 addingEventListener();
